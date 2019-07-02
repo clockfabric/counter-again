2e375cd73977c9fbb0730b20da5f243843c3b83d
test <test>
2019-07-02 16:06:33 -0400

    custom header messages added

--- src/App.js
+++ src/App.js
@@ -9,9 +9,22 @@ function App() {
 		setNumberOfCounters(count);
 	};
 
+	const renderHeaderMessage = () => {
+		switch (numberOfCounters) {
+			case 0:
+				return "No counters!";
+			case 1:
+				return "Only one counter!";
+			case 5:
+				return "Five counters, Yayyyyy!";
+			default:
+				return `Number of counters: ${numberOfCounters}`;
+		}
+	};
+
 	return (
 		<div className='App'>
-			<h1 className='m-5'>Number of counters: {numberOfCounters}</h1>
+			<h1 className='m-5'>{renderHeaderMessage()}</h1>
 			<Counters updateNumberOfCounters={updateNumberOfCounters} />
 		</div>
 	);
